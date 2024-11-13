49549efce7c40eae545c1becf8ebff1952eacba1
Gasana Shema <shemaphilbert8@gmail.com>
2024-11-13 16:52:14 +0200

    Human Readable Time

--- Human readable time.js
+++ Human readable time.js
@@ -1,3 +1,9 @@
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
 function humanReadable (seconds) {
   let hours = (seconds/3600)<10?"0"+(parseInt(seconds/3600)):parseInt(seconds/3600);
   let remainingSecs = seconds%3600;
