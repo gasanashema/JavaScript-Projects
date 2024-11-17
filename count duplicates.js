298d5d8538b8a4b9ebea14c2c48d4a01cb2086ce
Gasana Shema <shemaphilbert8@gmail.com>
2024-11-17 18:53:10 +0200

    Count Duplicates

--- count duplicates.js
+++ count duplicates.js
@@ -1,3 +1,21 @@
+/*
+
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+*/
+
+
+
 function duplicateCount(text) {
   let letterCheck = []; // To store characters that are already checked
   let result = []; // To store the final output
@@ -27,4 +45,12 @@ function duplicateCount(text) {
   return result.join("\n");
 }
 
-console.log(duplicateCount("Indivisibilities"));
+console.log(duplicateCount(""));
+
+/*
+
+result from codewars
+function duplicateCount(text){
+  return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
+*/
