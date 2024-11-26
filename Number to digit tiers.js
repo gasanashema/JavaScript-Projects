35706e37c8f3c8e6058e561ad5295e67450d1e9b
gasanashema <shemaphilbert8@gmail.com>
2024-11-26 11:13:08 +0200

    Number of digit tiers

--- Number to digit tiers.js
+++ Number to digit tiers.js
@@ -11,3 +11,21 @@ Examples
 4020 should return ["4", "40", "402", "4020"]
 80200 should return ["8", "80", "802", "8020", "80200"]
  */
+
+function numberDigitTiers(num){
+
+    let strNum = num.toString().split("");
+    let newArr = [];
+    for (let i = 0;i<strNum.length;i++){
+        let letter = [];
+        for (let j = 0;j<=i;j++){
+            letter.push(strNum[j]);
+        }
+        newArr.push(letter.join(''));
+    }
+
+
+    return newArr;
+}
+
+console.log(numberDigitTiers(80200));
