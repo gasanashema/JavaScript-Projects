fcc935d60c7b935d95c707d2e736220d3fe2972f
Shema <shemaphilbert8@gmail.com>
2024-12-20 11:06:00 +0200

    Finished adding li element

---
+++ Todo App/script.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+ let form = document.querySelector("#todoForm");
+ 
+ form.addEventListener("submit", addTodo);
+
+ function addTodo(event) {
+   event.preventDefault();
+
+   let inputValue = form.querySelector("input").value;
+   let ul = document.getElementById("todoList");
+   if (!inputValue.trim()) return;
+
+   
+   let listItem = document.createElement("li");
+   listItem.classList.add("list-group-item");
+
+   let formCheck = document.createElement('div');
+   formCheck.classList.add("form-check");
+
+
+   let checkBox = document.createElement("input");
+   checkBox.classList.add("form-check-input");
+   checkBox.type = "checkbox";
+
+   formCheck.appendChild(checkBox);
+
+   let label = document.createElement("label");
+   label.classList.add("form-check-label");
+   label.textContent = inputValue;
+
+   formCheck.appendChild(label);
+
+   let deleteBtn = document.createElement("button");
+   deleteBtn.classList.add("btn", "btn-outline-danger", "float-end");
+
+   deleteBtn.textContent = "X";
+   formCheck.appendChild(deleteBtn);
+console.log(ul);
+    listItem.appendChild(formCheck);
+   ul.appendChild(listItem);
+
+   form.querySelector("input").value = "";
+   // deleteBtn.addEventListener('click',removeElement);
+ }
+
+ let ul = document.getElementById("todoList");
+
+ ul.addEventListener('click',removeElement);
+
+ function removeElement(event) {
+    if (event.target.tagName = 'BUTTON') {
+
+        
+    }
+    
+ }
+
+});
