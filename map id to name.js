781c145d34b056ec13485a82608694f534941a9b
Shema <shemaphilbert8@gmail.com>
2025-02-26 05:35:57 +0200

    map id to name

---
+++ map id to name.js
@@ -0,0 +1,20 @@
+/**
+ * Write a function that takes an array of objects, where each object has an id and a name property. The function should return a new object where the keys are the id values and the values are the corresponding name values.
+ */
+const mapIdToName = (obj)=>{
+    const result = {};
+
+    for (const {id,name} of obj) {
+        result[id] = name;   
+    }
+
+    return result;
+}
+
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
+console.log(mapIdToName(users));
