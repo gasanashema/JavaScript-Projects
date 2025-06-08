dfe5a53bb622caa0bcd37dab8b35ce2f96638915
Shema <shemaphilbert8@gmail.com>
2025-06-08 14:15:15 +0200

    Fetch, fail, try again.js

--- AJAX/Fetch, fail, try again.js
+++ AJAX/Fetch, fail, try again.js
@@ -2,4 +2,24 @@
  * 
  */
 
-let api = "https://quotesondesign.com/wp-json/wp/v2/posts";
+let api = "https://quotesondesign.com/wp-json/wp/v2/psts";
+async function fetchQuote(url) {
+    const response = await fetch(url);
+    let i = 1;
+    while (!response.ok) {
+        if(i<=5){
+            setTimeout(async()=>response = await fetch(url),1000);
+            i++;
+            console.log(i);
+        }else{
+            throw Error('Invalid API');
+        }
+
+    }
+    const res = await response.json()
+    console.log(res[0]);
+
+    return res;
+}
+
+fetchQuote(api)//.then(console.log)
