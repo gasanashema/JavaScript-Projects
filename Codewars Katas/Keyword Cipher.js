fdf9567c3e86c4639b218fce067e614d7c14e510
Shema <shemaphilbert8@gmail.com>
2025-06-22 12:00:00 +0200

    feat: commit for June 22, 2025

--- Codewars Katas/Keyword Cipher.js
+++ Codewars Katas/Keyword Cipher.js
@@ -1,8 +1,11 @@
 function keywordCipher(string,keyword){
- const nonDuplicated = [... new Set(keyword.replace(/[^a-zA-Z ]/g, "").toLowerCase().split(''))].join('');
+//  const nonDuplicated = [...new Set(keyword.split(' ').map(a=>a.replace(/[^a-zA-Z ]/g, "")).join(' ').toLowerCase().split(''))].join('');
+ const nonDuplicated = [...new Set(keyword.split(' ').map(a=>a.replace(/[^a-zA-Z ]/g, "")).join(' ').toLowerCase().split(''))].join('');
+ const nonDuplicated = 
  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
  const cipher = `${nonDuplicated}${alphabet.split('').filter(a=> !nonDuplicated.includes(a)).join('')}`;
  const resArr = string.split('').map(a=>alphabet.indexOf(a)).map(a=>cipher.charAt(a)).join('');
  return resArr;
 }
-console.log(keywordCipher('hello','weDnesd@1231@3!$ay'));
+// console.log(keywordCipher('Welcome home','secret'));
+console.log(keywordCipher('hello','wednesday'));
